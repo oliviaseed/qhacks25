0e3df528dc27fa7b42fc1ef6a554fd9137632547
Olivia <olivia2193249@gmail.com>
2025-01-25 12:39:34 -0500

    Add more parameter fields to User and House

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from bson import ObjectId
 from datetime import datetime
 from .services.misc_services import encode_img
 
-USER_REQUIRED_FIELDS = ["username", "email", "password", "school", "age", "gender"]
-HOUSE_REQUIRED_FIELDS = ["type", "rooms_available", "rent", "utilities_included"]
+USER_REQUIRED_FIELDS = ["first_name", "last_name", "email", "password", "school", "age", "gender"]
+HOUSE_REQUIRED_FIELDS = ["type", "rooms_available", "rent", "utilities_included", "bathrooms", "address", "city", "province", "lease_length", "available_from"]
 
 class User:
     def __init__(self, db):
@@ -13,7 +13,6 @@ class User:
         """
         Create a new user document in the database.
         """
-        #TODO: preferences, etc.
 
         profile_picture_encoded = None
         if "profile_picture" in user_data and user_data["profile_picture"]:
@@ -24,12 +23,14 @@ class User:
                 images_encoded.append(encode_img(image_path))
 
         user = {
-            "username": user_data["username"],
+            "first_name": user_data["first_name"],
+            "last_name": user_data["last_name"],
             "email": user_data["email"],
-            "password": user_data["password"],  # Hash password in production
+            "password": user_data["password"],  # TODO: Hash password
             "school": user_data["school"],
             "age": user_data["age"],
             "gender": user_data["gender"],
+            "bio": user_data["bio"],
             "is_listing": user_data.get("is_listing", False),
             "house_id": None,
             "swipes": [],
@@ -75,6 +76,21 @@ class House:
             "rooms_available": house_data["rooms_available"],
             "rent": house_data["rent"],
             "utilities_included": house_data["utilities_included"],
+            "utilities_cost": house_data["utilities_cost"],
+            "bathrooms": house_data["bathrooms"],
+            "address": house_data["address"],
+            "city": house_data["city"],
+            "province": house_data["province"],
+            "parking": house_data["parking"],
+            "pets": house_data["pets"],
+            "smoking": house_data["smoking"],
+            "furnished": house_data["furnished"],
+            "laundry": house_data["laundry"],
+            "air_conditioning": house_data["air_conditioning"],
+            "dishwasher": house_data["dishwasher"],
+            "description": house_data["description"],
+            "lease_length": house_data["lease_length"],
+            "available_from": house_data["available_from"],
             "images": images_encoded,
             "created_at": datetime.utcnow()
         }
